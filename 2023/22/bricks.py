fd48dda636395ef2fe123f66d998a5fe2aae749b
Nate-Perkins <perkinsnathan97@gmail.com>
2023-12-30 16:52:01 -0500

    start day 22

--- 2023/22/bricks.py
+++ 2023/22/bricks.py
@@ -1,6 +1,18 @@
+dimensions = ["X", "Y", "Z"]
+
+bricks = []
 with open("D:\\Code\\advent-of-code\\advent-of-code\\2023\\22\\test.txt", "r") as file:
     lines = list(file)
-    for i in range(len(lines)):
-        lines[i] = lines[i].rstrip("\n")
-        line = lines[i]
-        for j in range(len(line)):
+    for line in lines:
+        line = line.rstrip("\n")
+        coords = line.split("~")
+        c1 = coords[0].split(",")
+        c2 = coords[1].split(",")
+        brick = {}
+        brick["X"] = (int(c1[0]), int(c2[0]))
+        brick["Y"] = (int(c1[1]), int(c2[1]))
+        brick["Z"] = (int(c1[2]), int(c2[2]))
+        bricks.append(brick)
+
+for b in bricks:
+    print(b)
